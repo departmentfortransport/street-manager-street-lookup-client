b9fd99dc327c8c00ec96ed550f06583746a9e204
Catherine Paul <c.paul@kainos.com>
2018-10-16 15:24:10 +0100

    Updated getStreet endpoint to include requestId

--- dist/app/client.d.ts
+++ dist/app/client.d.ts
@@ -8,6 +8,7 @@ export declare class StreetManagerStreetLookupClient {
     private axios;
     constructor(config: StreetManagerStreetLookupClientConfig);
     isAvailable(): Promise<boolean>;
-    getStreet(easting: number, northing: number): Promise<StreetResponse>;
+    getStreet(requestId: string, easting: number, northing: number): Promise<StreetResponse>;
     private httpHandler;
+    private generateRequestConfig;
 }
