ce15c5cb4e5176ec67b00720c7fe90c079567ed4
Joshua Higginson <joshuah@kainos.com>
2019-01-28 11:09:58 +0000

    SM-1705 update for timing changes

--- dist/interfaces/additionalSpecialDesignationsResponse.d.ts
+++ dist/interfaces/additionalSpecialDesignationsResponse.d.ts
@@ -2,4 +2,5 @@ export interface AdditionalSpecialDesignationsResponse {
     street_special_desig_code: string;
     special_desig_location_text: string;
     special_desig_description: string;
+    special_desig_periodicity_code: string;
 }
