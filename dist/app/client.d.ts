9ad31113b9663512aa73cbdd052c9a5a46cc32af
Catherine <c.paul@kainos.com>
2019-09-17 11:30:28 +0200

    SM-3645 - add new search by street endpoint

--- dist/app/client.d.ts
+++ dist/app/client.d.ts
@@ -1,5 +1,6 @@
 import { StreetResponse } from '../interfaces/streetResponse';
 import { RequestConfig } from '../interfaces/requestConfig';
+import { StreetSummaryResponse } from '../interfaces/streetSummaryResponse';
 export interface StreetManagerStreetLookupClientConfig {
     baseURL: string;
     timeout?: number;
@@ -11,6 +12,7 @@ export declare class StreetManagerStreetLookupClient {
     status(): Promise<void>;
     getStreets(config: RequestConfig, easting: number, northing: number): Promise<StreetResponse[]>;
     getStreetsByUsrn(config: RequestConfig, usrn: number): Promise<StreetResponse>;
+    getStreetsByQuery(config: RequestConfig, query: string): Promise<StreetSummaryResponse[]>;
     private httpHandler;
     private handleError;
     private generateRequestConfig;
