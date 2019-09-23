616f1abf7836560212e3a57699f7ccc25ef365fb
Richard Eustace <r.eustace@kainos.com>
2019-09-23 17:36:37 +0100

    feature/SM-3065-Add additional properties to ASD

---
+++ src/interfaces/referenceTypes.ts
@@ -0,0 +1,15 @@
+export enum AdditionalSpecialDesignationsPeriodicityCode {
+  everyday = 1,
+  working_days_only = 2,
+  weekends = 3,
+  monday_only = 7,
+  tuesday_only = 8,
+  wednesday_only = 9,
+  thursday_only = 10,
+  friday_only = 11,
+  saturday_only = 12,
+  sunday_only = 13,
+  public_and_bank_holidays = 14,
+  continuous_62 = 15,
+  special_arrangements_63 = 16
+}
