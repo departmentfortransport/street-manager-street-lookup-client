375fb5e17ddcff535174a2fe2e660f9f80b6c2d6
Agnieszka Nieznanska <a.nieznanska@kainos.com>
2019-12-09 10:53:44 +0000

    [SM-4323] special_desig_location_text is optional

--- src/interfaces/additionalSpecialDesignationsResponse.ts
+++ src/interfaces/additionalSpecialDesignationsResponse.ts
@@ -2,7 +2,7 @@ import { AdditionalSpecialDesignationCode, AdditionalSpecialDesignationsPeriodic
 
 export interface AdditionalSpecialDesignationsResponse {
   street_special_desig_code: AdditionalSpecialDesignationCode
-  special_desig_location_text: string
+  special_desig_location_text?: string
   special_desig_description: string
   special_desig_start_time?: number
   special_desig_end_time?: number
