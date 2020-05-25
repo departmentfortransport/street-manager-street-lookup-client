73a6c197fcbf929ff4274af26e563348656a9a9c
Catherine <c.paul@kainos.com>
2020-05-25 09:12:19 +0100

    SM-5613 - make asd description optional in documentation

--- dist/interfaces/additionalSpecialDesignationsResponse.d.ts
+++ dist/interfaces/additionalSpecialDesignationsResponse.d.ts
@@ -3,7 +3,7 @@ import { GeoJSONGeometry } from './geojsonTypes';
 export interface AdditionalSpecialDesignationsResponse {
     street_special_desig_code: AdditionalSpecialDesignationCode;
     special_desig_location_text?: string;
-    special_desig_description: string;
+    special_desig_description?: string;
     special_desig_start_time?: number;
     special_desig_end_time?: number;
     special_desig_periodicity_code?: AdditionalSpecialDesignationsPeriodicityCode;
